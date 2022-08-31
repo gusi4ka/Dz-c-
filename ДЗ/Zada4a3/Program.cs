123cb9eeede58b447f56c61dbaada1c2a99c3e34
gusi4ka <112167073+gusi4ka@users.noreply.github.com>
2022-08-31 17:23:21 +0300

    Add files via upload

---
+++ ДЗ/Zada4a3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 ==1)
+{
+
+Console.WriteLine("Число " + num + " Нечетное");
+}
+else
+{
+Console.WriteLine("Число " + num + " Четное");
+}
---
+++ ДЗ/zada4a2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Console.WriteLine("Введите первое число: ");
+        int num_A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+        int num_B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+        int num_C = Convert.ToInt32(Console.ReadLine());
+        int max = num_A;
+if (num_B > max)
+        {
+            max = num_B;
+        }
+if (num_C > max)
+        {
+            max = num_C;
+        }
+        Console.WriteLine("Наибольшее число: " + max);
